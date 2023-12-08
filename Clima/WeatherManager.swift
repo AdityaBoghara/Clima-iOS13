89391d465fd9798187aa6100590e92e5cd029a51
AdityaBoghara <bogharaaditya@gmail.com>
2023-12-08 15:03:44 -0700

    Using the URLSession for Networking

--- Clima/WeatherManager.swift
+++ Clima/WeatherManager.swift
@@ -15,6 +15,44 @@ struct WeatherManager {
     
     func fetchWeather(cityName: String) {
         let urlString = "\(weatherURL)&q=\(cityName)"
+        performRequest(urlString: urlString)
     }
     
+    
+    func performRequest(urlString :String) {
+        
+//        create URL
+        
+        if let url = URL(string: urlString){
+            
+//            create session
+            let session = URLSession(configuration: .default)
+            
+//            give session a task
+            let task = session.dataTask(with: url, completionHandler: handle(data:response:error:))
+            
+//            start the task
+            task.resume()
+        }
+        
+    }
+    
+    func handle(data: Data?, response: URLResponse?, error: Error?){
+        
+        if error != nil{
+            print(error!)
+            return
+        }
+        
+        if let safeData = data {
+            
+            let dataString = String(data: safeData, encoding: .utf8)
+            print(dataString)
+        }
+        
+        
+        
+        
+    }
+
 }
